8106b3b8502b2e37d3e6875e0477bb9379612c4d
Mikhail Podolskiy <mikhail_podolskiy@epam.com>
2018-12-12 12:10:52 +0600

    update package-info for decorator

--- src/main/java/com/mix/patterns/gof/structural/decorator/package-info.java
+++ src/main/java/com/mix/patterns/gof/structural/decorator/package-info.java
@@ -5,6 +5,8 @@ APPLICABILITY
 
 - To add responsibilities to individual objects dynamically and transparently, that is, without affecting other objects
 - For responsibilities that can be withdrawn
-- When extension by subclassing is impractical. Sometimes a large number of independent extensions are possible and would produce an explosion of subclasses to support every combination. Or a class definition may be hidden or otherwise unavailable for subclassing
+- When extension by subclassing is impractical.
+Sometimes a large number of independent extensions are possible and would produce an explosion of subclasses to support every combination.
+Or a class definition may be hidden or otherwise unavailable for subclassing
 
  */
