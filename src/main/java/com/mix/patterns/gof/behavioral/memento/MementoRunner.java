d061b0c8b1b638aa29e300867a8cd306f8df70bf
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2018-12-14 22:02:47 +0600

    gof : behavioral : memento

---
+++ src/main/java/com/mix/patterns/gof/behavioral/memento/MementoRunner.java
@@ -0,0 +1,19 @@
+package com.mix.patterns.gof.behavioral.memento;
+
+public class MementoRunner {
+
+    public static void main(String[] args) {
+        Originator originator = new Originator();
+        Caretaker caretaker = new Caretaker();
+
+        originator.setState("on");
+        System.out.printf("State is %s\n", originator.getState());
+        caretaker.setMemento(originator.saveState());
+
+        originator.setState("off");
+        System.out.printf("State is %s\n", originator.getState());
+
+        originator.restoreState(caretaker.getMemento());
+        System.out.printf("State is %s\n", originator.getState());
+    }
+}
---
+++ src/main/java/com/mix/patterns/gof/behavioral/memento/package-info.java
@@ -0,0 +1,9 @@
+package com.mix.patterns.gof.behavioral.memento;
+
+/*
+APPLICABILITY
+
+- a snapshot of an object's state must be saved so that it can be restored to that state later, and
+- a direct interface to obtaining the state would expose implementation details and break the object's encapsulation
+
+ */
